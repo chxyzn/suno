d1f92c25c160b50e61120b000ee821ee00bc8769
Chayan <chayanhjain@gmail.com>
2024-08-25 20:19:44 +0530

    feat: added navigation to player

--- app/(tabs)/(home)/home.tsx
+++ app/(tabs)/(home)/home.tsx
@@ -7,16 +7,20 @@ import {
   TextInput,
   Image,
   FlatList,
+  TouchableOpacity,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   verticalScale,
   horizontalScale,
   moderateScale,
-} from "../../utils/screen";
-import { COLORS } from "../../utils/colors";
+} from "../../../utils/screen";
+import { COLORS } from "../../../utils/colors";
+import { Router, useRouter } from "expo-router";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <SafeAreaView>
       <View style={scrollViewStyle.container}>
@@ -32,6 +36,7 @@ export default function Home() {
             { name: "Song 7", singer: "Singer 7", count: "7" },
             { name: "Song 8", singer: "Singer 8", count: "8" },
           ]}
+          router={router}
         />
         <RecommenededForYou
           songs={[
@@ -44,6 +49,7 @@ export default function Home() {
             { name: "Song 7", singer: "Singer 7", count: "7" },
             { name: "Song 8", singer: "Singer 8", count: "8" },
           ]}
+          router={router}
         />
       </View>
     </SafeAreaView>
@@ -74,7 +80,13 @@ const AppBar = () => {
   );
 };
 
-const RecentlyPlayed = ({ songs }: { songs: Song[] }) => {
+const RecentlyPlayed = ({
+  songs,
+  router,
+}: {
+  songs: Song[];
+  router: Router;
+}) => {
   console.log(songs);
   return (
     <View style={{ marginLeft: horizontalScale(12) }}>
@@ -90,42 +102,63 @@ const RecentlyPlayed = ({ songs }: { songs: Song[] }) => {
       <FlatList
         horizontal
         data={songs}
-        renderItem={({ item }) => <RecentlyPlayedItem song={item} />}
+        renderItem={({ item }) => (
+          <RecentlyPlayedItem song={item} router={router} />
+        )}
         keyExtractor={(item, index) => index.toString()}
       ></FlatList>
     </View>
   );
 };
 
-const RecentlyPlayedItem = ({ song }: { song: Song }) => {
+const RecentlyPlayedItem = ({
+  song,
+  router,
+}: {
+  song: Song;
+  router: Router;
+}) => {
   return (
-    <View style={{ marginRight: horizontalScale(10) }}>
-      <Image
-        source={{
-          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3FYPDsy6JPufPoVlaNW9RSjX_AD4e5pEevQ&s",
-        }}
-        style={{
-          resizeMode: "contain",
-          width: horizontalScale(130),
-          borderRadius: moderateScale(12),
-          height: verticalScale(130),
-        }}
-      ></Image>
-      <Text
-        style={{
-          fontSize: moderateScale(18),
-          color: COLORS.white,
-          alignSelf: "center",
-          marginTop: verticalScale(6),
-        }}
-      >
-        {song.name}
-      </Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => {
+        console.log("Pressed");
+        router.push("player");
+      }}
+    >
+      <View style={{ marginRight: horizontalScale(10) }}>
+        <Image
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3FYPDsy6JPufPoVlaNW9RSjX_AD4e5pEevQ&s",
+          }}
+          style={{
+            resizeMode: "contain",
+            width: horizontalScale(130),
+            borderRadius: moderateScale(12),
+            height: verticalScale(130),
+          }}
+        ></Image>
+        <Text
+          style={{
+            fontSize: moderateScale(18),
+            color: COLORS.white,
+            alignSelf: "center",
+            marginTop: verticalScale(6),
+          }}
+        >
+          {song.name}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
-const RecommenededForYou = ({ songs }: { songs: Song[] }) => {
+const RecommenededForYou = ({
+  songs,
+  router,
+}: {
+  songs: Song[];
+  router: Router;
+}) => {
   return (
     <View
       style={{
@@ -144,47 +177,61 @@ const RecommenededForYou = ({ songs }: { songs: Song[] }) => {
       </Text>
       <FlatList
         data={songs}
-        renderItem={({ item }) => <RecommenededForYouItem song={item} />}
+        renderItem={({ item }) => (
+          <RecommenededForYouItem song={item} router={router} />
+        )}
         keyExtractor={(item, index) => index.toString()}
       ></FlatList>
     </View>
   );
 };
 
-const RecommenededForYouItem = ({ song }: { song: Song }) => {
+const RecommenededForYouItem = ({
+  song,
+  router,
+}: {
+  song: Song;
+  router: Router;
+}) => {
   return (
-    <View
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        marginBottom: verticalScale(18),
+    <TouchableOpacity
+      onPress={() => {
+        router.push("player");
       }}
     >
-      <Image
-        source={{
-          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3FYPDsy6JPufPoVlaNW9RSjX_AD4e5pEevQ&s",
-        }}
-        style={{
-          resizeMode: "contain",
-          width: horizontalScale(130),
-          height: verticalScale(130),
-          borderRadius: moderateScale(12),
-          marginRight: horizontalScale(12),
-        }}
-      />
       <View
         style={{
           display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          justifyContent: "center",
+          flexDirection: "row",
+          marginBottom: verticalScale(18),
         }}
       >
-        <Text>{song.name}</Text>
-        <Text>{song.singer}</Text>
-        <Text>{song.count + " streams"}</Text>
+        <Image
+          source={{
+            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3FYPDsy6JPufPoVlaNW9RSjX_AD4e5pEevQ&s",
+          }}
+          style={{
+            resizeMode: "contain",
+            width: horizontalScale(130),
+            height: verticalScale(130),
+            borderRadius: moderateScale(12),
+            marginRight: horizontalScale(12),
+          }}
+        />
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+          }}
+        >
+          <Text>{song.name}</Text>
+          <Text>{song.singer}</Text>
+          <Text>{song.count + " streams"}</Text>
+        </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from "expo-router";
 export default function NavTab() {
   return (
     <Tabs>
-      <Tabs.Screen name="home" options={{ headerShown: false }} />
+      <Tabs.Screen name="(home)" options={{ headerShown: false }} />
       <Tabs.Screen name="search" options={{ headerShown: false }} />
       <Tabs.Screen name="library" options={{ headerShown: false }} />
     </Tabs>
